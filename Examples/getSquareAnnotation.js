1d18d97b3c84770c28b71e1a6e97afc78bcf0eeb
Mateen Sajjad <mateen.sajjad@aspose.com>
2019-07-11 01:43:28 +0500

    Updated code and removed private keys

--- Examples/getSquareAnnotation.js
+++ Examples/getSquareAnnotation.js
@@ -2,8 +2,8 @@ const { PdfApi } = require("asposepdfcloud");
 const { SoundAnnotation } = require("asposepdfcloud/src/models/soundAnnotation");
 
 
-pdfApi = new PdfApi(
-    "78946fb4-3bd4-4d3e-b309-f9e2ff9ac6f9", "b125f13bf6b76ed81ee990142d841195")
+
+pdfApi = new PdfApi("XXXX", "XXXXXXX")
 
 console.log('running example');
 
--- Examples/putPageConvertToPng.js
+++ Examples/putPageConvertToPng.js
@@ -2,9 +2,7 @@ const { PdfApi } = require("asposepdfcloud");
 const { PdfAType } = require("asposepdfcloud/src/models/movieAnnotation");
 
 
-pdfApi = new PdfApi(
-    "78946fb4-3bd4-4d3e-b309-f9e2ff9ac6f9", "b125f13bf6b76ed81ee990142d841195")
-
+pdfApi = new PdfApi("XXXX", "XXXXXXX")
 console.log('running example');
 
 
