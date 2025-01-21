30095598407dadc1c1c9a8a02ece7d32db78079e
Dmitriy-Xawstov <dmytry.xawstov@aspose.com>
2025-01-21 14:49:41 +0200

    attachments-uses-cases

---
+++ UsesCases/Attachments/get/get.js
@@ -0,0 +1,57 @@
+/**************************************************************************************************
+    1. Load your Application Secret and Key from the JSON file or set credentials in another way
+    2. Create a Storage API object for uploading Pdf file
+    3. Create an object to connect to the Pdf.Cloud API
+    4. Upload your document file using PutCreate() function of the Storage API object
+    5. To extract attachments from a PDF document, using getDocumentAttachments() function
+    6. Check result and perform some actions with result.body object
+        in format (Attachments)[https://github.com/aspose-pdf-cloud/aspose-pdf-cloud-node.js/blob/master/docs/Attachments.md]
+    
+    All values of variables starting with "YOUR_****" should be replaced by real user values
+****************************************************************************************************/
+import credentials from "./credentials.json"  with { type: "json" };
+import fs from 'node:fs/promises';
+import { PdfApi } from "asposepdfcloud";
+
+const LOCAL_PATH = "C:\\Samples\\";
+const FILE_NAME_SHORT = "Sample-Document";
+const FILE_NAME = FILE_NAME_SHORT + ".pdf";
+
+const pdfApi = new PdfApi(credentials.id, credentials.key);
+
+async function ProcessPdfAttachments(attachments)
+{
+    if (attachments != null && attachments != undefined)
+    {
+        await Promise.all(
+            attachments.map(async (attachment, index) => {
+                const downloadRes = await pdfApi.downloadFile(attachment.href);
+                await fs.writeFile( LOCAL_PATH + FILE_NAME_SHORT + "_attachment_" + (index + 1) + ".data", downloadRes.body);
+            })
+        );
+    }
+    else
+        console.log("Unexpected error : can't download attachments");
+}
+
+async function getPdfDocumentAttachments(api, pdfFilePath, pdfFileName)
+{
+    // Null value means default storage
+    let storage = null;
+
+    const pdfFileData = await fs.readFile(pdfFilePath + pdfFileName);
+
+    await api.uploadFile(pdfFileName, pdfFileData, storage);
+
+    let result = await pdfApi.getDocumentAttachments(pdfFileName, storage, null);
+    
+    if (result.statusCode == 200)
+    {
+        const attachments = result.body;
+				
+        await ProcessPdfAttachments(attachments);
+    }
+
+}
+
+await getPdfDocumentAttachments(pdfApi, LOCAL_PATH, FILE_NAME);
