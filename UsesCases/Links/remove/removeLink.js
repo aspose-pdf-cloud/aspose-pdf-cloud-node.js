5f5e3ef165438fb518615c16c9933303e55081bf
Dmitriy-Xawstov <dmytry.xawstov@aspose.com>
2025-02-07 02:20:54 +0200

    Add files via upload

---
+++ UsesCases/Links/remove/removeLink.js
@@ -0,0 +1,60 @@
+// 1. Load your Application Secret and Key from the JSON file or set credentials in another way
+// 2. Create an object to connect to the Pdf.Cloud API
+// 3. Upload your document file
+// 4. Delete required Link Annotation from the document using deleteLinkAnnotation() function
+// 6. Perform some action after successful removing the Link Annotation from document
+// All values of variables starting with "YOUR_****" should be replaced by real user values
+
+import credentials from "../../../../Credentials/credentials.json"  with { type: "json" };
+import fs from 'node:fs/promises';
+import path from 'node:path';
+import { PdfApi } from "../../../src/api/api.js";
+
+const configParams = {
+    LOCAL_FOLDER: "C:\\Samples\\",
+    PDF_DOCUMENT_NAME: "sample.pdf",
+    LOCAL_RESULT_DOCUMENT_NAME: "output_sample.pdf",
+    LINK_REMOVE_ID: "GI5UO32UN5KVESKBMN2GS33OHMZTEMJMGUYDQLBTGYYCYNJSGE",
+    PAGE_NUMBER: 2,     // Your document page number...
+};
+
+const pdfApi = new PdfApi(credentials.id, credentials.key);
+
+const pdfLinks = {
+   async uploadDocument() {
+        const pdfFilePath = path.join(configParams.LOCAL_FOLDER, configParams.PDF_DOCUMENT_NAME);
+        const pdfFileData = await fs.readFile(pdfFilePath);
+        await pdfApi.uploadFile(configParams.PDF_DOCUMENT_NAME, pdfFileData);
+    },
+       
+    async downloadResult() {
+        const changedPdfData = await pdfApi.downloadFile(configParams.PDF_DOCUMENT_NAME);
+        const filePath = path.join(configParams.LOCAL_FOLDER, configParams.LOCAL_RESULT_DOCUMENT_NAME);
+        await fs.writeFile(filePath, changedPdfData.body);
+        console.log("Downloaded: " + filePath);
+    },
+
+    removeLink: async function () {
+        const resultDelete = await pdfApi.deleteLinkAnnotation(configParams.PDF_DOCUMENT_NAME, configParams.LINK_REMOVE_ID);
+
+        if (resultDelete.body.code == 200) {
+            console.log("Link '" + configParams.LINK_REMOVE_ID + "' was deleted!");
+            return ;
+        }
+        else
+            throw new Error("Unexpected error : can't get link !!!");
+    }
+
+}
+
+async function main() {
+    try {
+        await pdfLinks.uploadDocument();
+        await pdfLinks.removeLink();
+        await pdfLinks.downloadResult();
+    } catch (error) {
+        console.error("Error:", error.message);
+    }
+}
+
+main();
