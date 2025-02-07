d3df02d54728a8c5f3bba2333c305a46b6dd4024
Dmitriy-Xawstov <dmytry.xawstov@aspose.com>
2025-02-07 02:19:53 +0200

    Add files via upload

---
+++ UsesCases/Links/add/appendLink.js
@@ -0,0 +1,88 @@
+// 1. Load your Application Secret and Key from the JSON file or set credentials in another way
+// 2. Create an object to connect to the Pdf.Cloud API
+// 3. Upload your document file
+// 4. Create a new Link Annotation with the required properties
+// 5. Append new Link Annotation to the document using postPageLinkAnnotations() function
+// 6. Perform some action after successful addition
+// All values of variables starting with "YOUR_****" should be replaced by real user values
+
+import credentials from "../../../../Credentials/credentials.json"  with { type: "json" };
+import fs from 'node:fs/promises';
+import path from 'node:path';
+import { PdfApi } from "../../../src/api/api.js";
+import { Color } from "../../../src/models/color.js";
+import { Link } from "../../../src/models/link.js";
+import { Rectangle } from "../../../src/models/rectangle.js";
+import { LinkAnnotation } from "../../../src/models/linkAnnotation.js";
+import { LinkHighlightingMode} from "../../../src/models/linkHighlightingMode.js";
+import { LinkActionType } from "../../../src/models/linkActionType.js";
+
+const configParams = {
+    LOCAL_FOLDER: "C:\\Samples\\",
+    PDF_DOCUMENT_NAME: "sample.pdf",
+    LOCAL_RESULT_DOCUMENT_NAME: "output_sample.pdf",
+    NEW_LINK_ACTION: "https://reference.aspose.cloud/pdf/#/",
+    PAGE_NUMBER: 2,     // Your document page number...
+    LINK_POS_LLX: 244.914,
+    LINK_POS_LLY: 488.622,
+    LINK_POS_URX: 284.776,
+    LINK_POS_URY: 498.588,
+};
+
+const pdfApi = new PdfApi(credentials.id, credentials.key);
+
+const pdfLinks = {
+    async uploadDocument() {
+        const pdfFilePath = path.join(configParams.LOCAL_FOLDER, configParams.PDF_DOCUMENT_NAME);
+        const pdfFileData = await fs.readFile(pdfFilePath);
+        await pdfApi.uploadFile(configParams.PDF_DOCUMENT_NAME, pdfFileData);
+    },
+    
+    async downloadResult() {
+        const changedPdfData = await pdfApi.downloadFile(configParams.PDF_DOCUMENT_NAME);
+        const filePath = path.join(configParams.LOCAL_FOLDER, configParams.LOCAL_RESULT_DOCUMENT_NAME);
+        await fs.writeFile(filePath, changedPdfData.body);
+        console.log("Downloaded: " + filePath);
+    },
+
+    async appendLink () {
+
+        const linkColor = new Color({ a: 255, r: 0, g: 255, b: 0 });
+
+        const linkRectangle = new Rectangle();
+        linkRectangle.lLX = configParams.LINK_POS_LLX;
+        linkRectangle.lLY = configParams.LINK_POS_LLY;
+        linkRectangle.uRX = configParams.LINK_POS_URX;
+        linkRectangle.uRY = configParams.LINK_POS_URY;
+
+        const linkItem = new Link({ rel: "self" });
+
+        const newLink = new LinkAnnotation();
+        newLink.links = [ linkItem ];
+        newLink.actionType = LinkActionType.GoToURIAction,
+        newLink.action = configParams.NEW_LINK_ACTION,
+        newLink.highlighting = LinkHighlightingMode.Invert,
+        newLink.color = linkColor;
+        newLink.rect = linkRectangle;
+        
+        var addResponse = await pdfApi.postPageLinkAnnotations(configParams.PDF_DOCUMENT_NAME, configParams.PAGE_NUMBER, [ newLink ]);
+
+        if (addResponse.body.code == 200) {
+            console.log("Append link successful!");
+            return true;
+        }
+    },
+
+}
+
+async function main() {
+    try {
+        await pdfLinks.uploadDocument();
+        await pdfLinks.appendLink();
+        await pdfLinks.downloadResult();
+    } catch (error) {
+        console.error("Error:", error.message);
+    }
+}
+
+main();
