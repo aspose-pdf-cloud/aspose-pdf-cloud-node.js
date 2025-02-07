c92b99ccac49a726ce027d71710e903902138edf
Dmitriy-Xawstov <dmytry.xawstov@aspose.com>
2025-02-07 02:33:15 +0200

    Update removeBookmarks.js

--- UsesCases/Bookmarks/remove/removeBookmarks.js
+++ UsesCases/Bookmarks/remove/removeBookmarks.js
@@ -5,7 +5,7 @@
 // 6. Perform some action after successful removing the Bookmark from document
 // All values of variables starting with "YOUR_****" should be replaced by real user values
 
-import credentials from "credentials.json"  with { type: "json" };
+import credentials from "credentials.json"  with { type: "json" };    // json-file in this format: { "id": "*****", "key": "*******" }
 import fs from 'node:fs/promises';
 import path from 'node:path';
 import { PdfApi } from "asposepdfcloud";
