5ad4f2feb35ff8fba758cf6f51d833e47dc5c127
Dmitriy-Xawstov <dmytry.xawstov@aspose.com>
2025-04-22 11:17:11 +0300

    Update getTablesAndShow.js

--- UsesCases/Tables/get/getTablesAndShow.js
+++ UsesCases/Tables/get/getTablesAndShow.js
@@ -1,21 +1,13 @@
-// 1. Load your Application Secret and Key from the JSON file or set credentials in another way
-// 2. Create an object to connect to the Pdf.Cloud API
-// 3. Upload your document file
-// 4. Retrieve required Link Annotation from the document using getPageLinkAnnotation() function
-// 6. Perform some action after successful retrieving the Link Annotation from document
-// All values of variables starting with "YOUR_****" should be replaced by real user values
-
-import credentials from "./credentials.json"  with { type: "json" };
+import credentials from "../../../../Credentials/credentials.json"  with { type: "json" };
 import fs from 'node:fs/promises';
 import path from 'node:path';
-import { PdfApi } from "asposepdfcloud";
+import { PdfApi } from "../../../src/api/api.js";
 
 const configParams = {
     LOCAL_FOLDER: "C:\\Samples\\",
     PDF_DOCUMENT_NAME: "sample.pdf",
-    LOCAL_RESULT_DOCUMENT_NAME: "output_sample.pdf",
-    PAGE_NUMBER: 2,     // Your document page number...
-    TABLE_ID: "GE5TCOZSGAYCYNRQGUWDINZVFQ3DGMA",
+    PAGE_NUMBER: 2,                                 // Your document page number...
+    TABLE_ID: "GE5TCOZSGAYCYNRQGUWDINZVFQ3DGMA",    // Your table id...
 };
 
 const pdfApi = new PdfApi(credentials.id, credentials.key);
@@ -34,7 +26,7 @@ const pdfTables = {
             if (!Array.isArray(resultTabs.body.tables.list) || resultTabs.body.tables.list.length === 0) {
                 throw new Error("Unexpected error : tables is null or empty!!!");
             }
-            this.showLTablesInfo(resultTabs.body.tables.list, "All tables");
+            this.showTablesInfo(resultTabs.body.tables.list, "All tables");
             return resultTabs.body.tables.list;
         }
         else
@@ -45,14 +37,14 @@ const pdfTables = {
         const resultTabs = await pdfApi.getTable(configParams.PDF_DOCUMENT_NAME, configParams.TABLE_ID);
 
         if (resultTabs.body.code == 200 && resultTabs.body.table) {
-            this.showLTablesInfo( [ resultTabs.body.table ], "Table by Id");
+            this.showTablesInfo( [ resultTabs.body.table ], "Table by Id");
             return resultTabs.body.table;
         }
         else
             console.error("Unexpected error : can't get links!!!");
     },
 
-    showLTablesInfo(tables, prefix) {
+    showTablesInfo(tables, prefix) {
         if (Array.isArray(tables) && tables.length > 0)
         {
             tables.forEach(function(table) {
