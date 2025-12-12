166b663d64f68a50bf92b77861ca4e1937aa690e
Kirill Novinskij <gitlab_admin_55cb3d@example.com>
2025-12-12 02:25:05 +0000

    sources generated v25.11.0

--- src/requestHelper.ts
+++ src/requestHelper.ts
@@ -95,7 +95,7 @@ async function invokeApiMethodInternal(requestOptions: request.Options, confgura
     //headers
     sa.set("User-Agent", "pdf nodejs sdk");
     sa.set("x-aspose-client", "nodejs sdk");  
-    sa.set("x-aspose-client-version", "25.10.0");
+    sa.set("x-aspose-client-version", "25.11.0");
 
     if (!requestOptions.headers) {
         requestOptions.headers = {};
